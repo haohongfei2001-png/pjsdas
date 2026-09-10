01ca2ed57a3f28c5e5a2e4eff2cec6f228dcbcd8
haohongfei2001-png <haohongfei2001@gmail.com>
2026-09-10 13:45:07 +0800

    Wire fixed-event confirmation guard

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './AppV5'
+import FixedEventGuard from './FixedEventGuard'
 import ProcessEventDock from './ProcessEventDock'
 import './styles.css'
 
 function Root() {
   const [revision, setRevision] = useState(0)
+  const refresh = () => setRevision((value) => value + 1)
   return (
     <>
       <App key={revision} />
-      <ProcessEventDock onChanged={() => setRevision((value) => value + 1)} />
+      <ProcessEventDock onChanged={refresh} />
+      <FixedEventGuard key={`fixed-${revision}`} onChanged={refresh} />
     </>
   )
 }
