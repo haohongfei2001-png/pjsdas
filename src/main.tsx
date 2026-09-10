89c9305a7abee270b27e5af0c76ee36fbcca7e0c
haohongfei2001-png <haohongfei2001@gmail.com>
2026-09-10 13:07:02 +0800

    Wire global process-event capture into app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './AppV4'
+import ProcessEventDock from './ProcessEventDock'
 import './styles.css'
+
+function Root() {
+  const [revision, setRevision] = useState(0)
+  return (
+    <>
+      <App key={revision} />
+      <ProcessEventDock onChanged={() => setRevision((value) => value + 1)} />
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
