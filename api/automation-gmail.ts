be268a68b28e0de90fe73080c138f01c3907fb11
haohongfei2001-png <haohongfei2001@gmail.com>
2026-09-15 02:05:12 +0800

    refactor: remove manual worker secrets from Vercel

--- api/automation-gmail.ts
+++ api/automation-gmail.ts
@@ -1,13 +1,15 @@
 import { createGmailAutomationHandler } from '../gateway/gmailAutomationHandler.js'
-import { PJSDAS_SUPABASE_URL } from '../gateway/supabaseProject.js'
+import {
+  PJSDAS_SUPABASE_PUBLISHABLE_KEY,
+  PJSDAS_SUPABASE_URL,
+} from '../gateway/supabaseProject.js'
 
 const handler = createGmailAutomationHandler({
   supabaseUrl: PJSDAS_SUPABASE_URL,
-  serviceRoleKey: process.env.PJSDAS_SUPABASE_SERVICE_ROLE_KEY ?? '',
+  supabasePublishableKey: PJSDAS_SUPABASE_PUBLISHABLE_KEY,
   tokenEncryptionKey: process.env.PJSDAS_TOKEN_ENCRYPTION_KEY ?? '',
   googleClientId: process.env.PJSDAS_GOOGLE_CLIENT_ID ?? '',
   googleClientSecret: process.env.PJSDAS_GOOGLE_CLIENT_SECRET ?? '',
-  automationSecret: process.env.PJSDAS_AUTOMATION_SECRET ?? process.env.CRON_SECRET ?? '',
 })
 
 export default {
