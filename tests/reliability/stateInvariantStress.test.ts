2ed45a5ef83f40909bec55b59eab59d9df284568
haohongfei2001-png <haohongfei2001@gmail.com>
2026-09-19 12:55:19 +0800

    test: mark reliability monitor fixtures source-verified

--- tests/reliability/stateInvariantStress.test.ts
+++ tests/reliability/stateInvariantStress.test.ts
@@ -36,6 +36,8 @@ function observation(input: {
     fitConfidence: 'high',
     opportunityValueConfidence: 'high',
     postingStatus: 'open',
+    sourceVerification: 'verified',
+    sourceVerifiedAt: '2026-09-14T08:00:00.000Z',
     discoveredAt: '2026-09-14T08:00:00.000Z',
   }
 }
