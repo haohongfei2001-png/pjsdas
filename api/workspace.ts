77a855abeddd2da9b362049394e7ad652c0e5c96
hongfei hao <haohongfei2001@gmail.com>
2026-09-19 18:06:32 +0800

    refactor: use shared first-party origin policy

--- api/workspace.ts
+++ api/workspace.ts
@@ -3,15 +3,13 @@ import {
   PJSDAS_SUPABASE_PUBLISHABLE_KEY,
   PJSDAS_SUPABASE_URL,
 } from '../gateway/supabaseProject.js'
+import { firstPartyWebOrigins } from '../gateway/productionTopology.js'
 
 const handler = createConnectedWorkspaceHandler({
   supabaseUrl: PJSDAS_SUPABASE_URL,
   supabasePublishableKey: PJSDAS_SUPABASE_PUBLISHABLE_KEY,
   serviceRoleKey: process.env.PJSDAS_SUPABASE_SERVICE_ROLE_KEY ?? '',
-  allowedOrigins: [
-    'https://haohongfei2001-png.github.io',
-    'http://localhost:5173',
-  ],
+  allowedOrigins: firstPartyWebOrigins(),
 })
 
 export default {
