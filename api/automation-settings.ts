9a55046d1faa6431f6bc0d56d936f08cbbbb39b5
hongfei hao <haohongfei2001@gmail.com>
2026-09-19 18:08:13 +0800

    feat: apply controlled audience guard to first-party API

--- api/automation-settings.ts
+++ api/automation-settings.ts
@@ -4,11 +4,13 @@ import {
   PJSDAS_SUPABASE_URL,
 } from '../gateway/supabaseProject.js'
 import { firstPartyWebOrigins } from '../gateway/productionTopology.js'
+import { createConfiguredAudienceAccessGuard } from '../gateway/audienceAccess.js'
 
 const handler = createAutomationSettingsHandler({
   supabaseUrl: PJSDAS_SUPABASE_URL,
   supabasePublishableKey: PJSDAS_SUPABASE_PUBLISHABLE_KEY,
   allowedOrigins: firstPartyWebOrigins(),
+  authorizeIdentity: createConfiguredAudienceAccessGuard({ supabaseUrl: PJSDAS_SUPABASE_URL }),
 })
 
 export default {
