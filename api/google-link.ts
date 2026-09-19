1f301d20a01cfeb2fdd989f5dd7f9876f0aff9f8
hongfei hao <haohongfei2001@gmail.com>
2026-09-19 18:08:07 +0800

    feat: apply controlled audience guard to first-party API

--- api/google-link.ts
+++ api/google-link.ts
@@ -4,12 +4,14 @@ import {
   PJSDAS_SUPABASE_URL,
 } from '../gateway/supabaseProject.js'
 import { firstPartyWebOrigins } from '../gateway/productionTopology.js'
+import { createConfiguredAudienceAccessGuard } from '../gateway/audienceAccess.js'
 
 const handler = createGoogleLinkHandler({
   supabaseUrl: PJSDAS_SUPABASE_URL,
   supabasePublishableKey: PJSDAS_SUPABASE_PUBLISHABLE_KEY,
   tokenEncryptionKey: process.env.PJSDAS_TOKEN_ENCRYPTION_KEY ?? '',
   allowedOrigins: firstPartyWebOrigins(),
+  authorizeIdentity: createConfiguredAudienceAccessGuard({ supabaseUrl: PJSDAS_SUPABASE_URL }),
 })
 
 export default {
