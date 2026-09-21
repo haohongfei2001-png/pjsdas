755deab3da63ad5d468af4986c71123bb39cebf9
hongfei hao <haohongfei2001@gmail.com>
2026-09-21 19:49:58 +0800

    feat(uu06): wire Gmail push topic into settings

--- api/automation-settings.ts
+++ api/automation-settings.ts
@@ -10,6 +10,10 @@ const handler = createAutomationSettingsHandler({
   supabaseUrl: PJSDAS_SUPABASE_URL,
   supabasePublishableKey: PJSDAS_SUPABASE_PUBLISHABLE_KEY,
   allowedOrigins: firstPartyWebOrigins(),
+  tokenEncryptionKey: process.env.PJSDAS_TOKEN_ENCRYPTION_KEY ?? '',
+  googleClientId: process.env.PJSDAS_GOOGLE_CLIENT_ID ?? '',
+  googleClientSecret: process.env.PJSDAS_GOOGLE_CLIENT_SECRET ?? '',
+  gmailPushTopicName: process.env.PJSDAS_GMAIL_PUBSUB_TOPIC ?? '',
   authorizeIdentity: createConfiguredAudienceAccessGuard({ supabaseUrl: PJSDAS_SUPABASE_URL }),
 })
 
