f8b678b74e8a6fba6fb655d61c716aa8d7508ddf
hongfei hao <haohongfei2001@gmail.com>
2026-09-22 16:46:09 +0800

    feat(uu06): make fenced Gmail execution default-on

--- api/automation-gmail.ts
+++ api/automation-gmail.ts
@@ -12,7 +12,7 @@ const automationHandler = createGmailAutomationHandler({
   tokenEncryptionKey: process.env.PJSDAS_TOKEN_ENCRYPTION_KEY ?? '',
   googleClientId: process.env.PJSDAS_GOOGLE_CLIENT_ID ?? '',
   googleClientSecret: process.env.PJSDAS_GOOGLE_CLIENT_SECRET ?? '',
-  executionControlsEnabled: process.env.PJSDAS_GMAIL_EXECUTION_CONTROLS === 'true',
+  executionControlsEnabled: process.env.PJSDAS_GMAIL_EXECUTION_CONTROLS !== 'false',
 })
 
 const watchHandler = createGmailWatchHandler({
@@ -31,7 +31,7 @@ const pushHandler = createGmailPushHandler({
   expectedAudience: process.env.PJSDAS_GMAIL_PUSH_AUDIENCE ?? '',
   expectedServiceAccountEmail: process.env.PJSDAS_GMAIL_PUSH_SERVICE_ACCOUNT_EMAIL ?? '',
   expectedSubscription: process.env.PJSDAS_GMAIL_PUBSUB_SUBSCRIPTION ?? '',
-  executionControlsEnabled: process.env.PJSDAS_GMAIL_EXECUTION_CONTROLS === 'true',
+  executionControlsEnabled: process.env.PJSDAS_GMAIL_EXECUTION_CONTROLS !== 'false',
 })
 
 export const gmailAutomationApi = { fetch: automationHandler }
